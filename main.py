1ffd97f8179fe1bb1a62d6f8774e37e6dc73c32e
Adrian Torres <adt@odoo.com>
2017-10-12 14:54:58 +0200

    Add basic functionality
    The program can retrieve the desired repos to be deleted
    and delete them automatically with the provided user input
    i.e. username and password.
    
    For now, the search query for the repos to be deleted is
    hardcoded into main.py

---
+++ main.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+
+def main(username, password):
+    # Init, access login page
+    driver = webdriver.Chrome()
+    driver.get("http://www.github.com/login")
+
+    # Login
+    usr = driver.find_element_by_name("login")
+    usr.clear()
+    usr.send_keys(username)
+
+    pwd = driver.find_element_by_name("password")
+    pwd.clear()
+    pwd.send_keys(password)
+    pwd.send_keys(Keys.RETURN)
+
+    # Find to-delete repos
+    # TODO: Generalize
+    driver.get(
+        "https://github.com/%s?tab=repositories&q=cb-gh&type=fork" % username
+    )
+
+    repos = set()
+
+    # TODO: Atomize code
+
+    while True:
+        p = driver.find_elements_by_xpath(
+            "//a[@itemprop='name codeRepository']"
+        )
+
+        for elem in p:
+            repos.add(elem.text)
+
+        try:
+            next = driver.find_element_by_xpath("//a[@class='next_page']")
+            next.click()
+        except Exception:
+            break
+
+    # TODO: Display the list of repos to be deleted and ask the user for
+    # confirmation before proceeding
+
+    i = 1
+    for repo in list(repos):
+        driver.get("https://github.com/%s/%s/settings" % (username, repo))
+        delete = driver.find_element_by_xpath(
+            "//button[text()='Delete this repository']"
+        )
+        delete.click()
+
+        # The verify modal is created on the fly, so we need to hack shit up
+        # in order to inject the repo's name into the input box
+        action = ActionChains(driver)
+        action.send_keys(repo + Keys.RETURN)
+        action.perform()
+
+        try:
+            pwd = driver.find_element_by_name("sudo_password")
+            pwd.send_keys(password)
+            pwd.send_keys(Keys.RETURN)
+        except Exception:
+            pass
+
+        max = len(repos)
+        print("Repository %s successfully deleted! (%d/%d)" % (repo, i, max))
+        i += 1
+
+    driver.quit()
+
+
+if __name__ == '__main__':
+    # TODO: Better authentication
+    main(*sys.argv[1:])
